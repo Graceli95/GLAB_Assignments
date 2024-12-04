654c4eec696c77ca395ae6c461a7d2ca48249fc1
Graceli95 <graceli9095@gmail.com>
2024-12-03 21:22:41 -0700

    add files

---
+++ src/GLAB_303_9_2_Basic_Inheritance_Example/Surgeon.java
@@ -0,0 +1,8 @@
+package GLAB_303_9_2_Basic_Inheritance_Example;
+
+public class Surgeon extends Doctor {
+   void Surgeon_Details(){
+        System.out.println("Surgeon Details...");
+        System.out.println(Department = "Cardio");
+    }
+}
