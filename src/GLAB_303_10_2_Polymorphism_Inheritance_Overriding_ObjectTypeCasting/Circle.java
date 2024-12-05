01579694222666bfd83532ddde93a33f8002b070
Graceli95 <graceli9095@gmail.com>
2024-12-04 21:18:25 -0700

    add files

---
+++ src/GLAB_303_10_2_Polymorphism_Inheritance_Overriding_ObjectTypeCasting/Circle.java
@@ -0,0 +1,32 @@
+package GLAB_303_10_2_Polymorphism_Inheritance_Overriding_ObjectTypeCasting;
+//Circle is a child class
+public class Circle extends Shape {
+    protected double radius;
+    private final double PI = Math.PI;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+    public double getArea(){
+        //double area = PI * this.radius * this.radius;
+        super.area = PI * Math.pow(this.radius, 2); //initializing value in parent class variable
+        return super.area; //reference to parent class variable
+    }
+
+    @Override
+    public void displayShapeName() {
+        System.out.println("Drawing a Circle of radius " + this.radius);
+    }
+    /** Returns a self-descriptive string */
+    @Override
+    public String toString() {
+        return "Circle [radius= " + radius + " " + super.toString() + "]";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
